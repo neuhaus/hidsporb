ed08f3b0513614b1d9422d2a24b1eb92680cca0b
vputz <vputz>
2002-04-19 02:01:20 +0000

    Minor, minor changes; added files for VPutz to set environment variables cleaned spurious ^M's from some files, added my (vputz's) include directories to SOURCES

---
+++ src/hidsporb/vicenv.cmd
@@ -0,0 +1,6 @@
+rem call "f:\dev\microsoft visual studio\vc98\bin\vcvars32.bat
+call g:\ntddk\bin\setenv.bat g:\ntddk free
+set NTDEBUG=ntsd
+set NTDEBUGTYPE=windbg
+set MSC_OPTIMIZATION=/Od
+cd \yak\hidsporb\src\hidsporb
